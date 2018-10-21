9cb31767d68c0ff172ae803f07c824fd33997fae
jdlrobson <jdlrobson@gmail.com>
2018-10-21 14:29:22 -0700

    Add footer with github link

--- components/Page/index.jsx
+++ components/Page/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Row from './../Row';
+import Column from './../Column';
 import './styles.less';
 
 export default class Page extends React.Component {
 	render() {
 		return (
 			<div className="page" data-title={this.props.title}>
-				<div className="page__contents">{this.props.children}</div>
+				<Row className="page__contents">{this.props.children}</Row>
+				<footer className="grid-row">
+					<Column col={2}>
+						Someday guide is a <a href="https://jdlrobson.com">Jon Robson</a> and Linz Lim production.
+					</Column>
+					<Column col={3}>
+						<a href="https://github.com/jdlrobson/someday">
+							<img width="40" height="40" src="/github.png" />
+						</a>
+					</Column>
+				</footer>
 			</div>
 		);
 	}
--- components/Row/index.jsx
+++ components/Row/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles.less';
 
-export default function Row( { children } ) {
+export default function Row( { children, row = 1 } ) {
 	return (
-		<div className="grid-row">
+		<div className={"grid-row " + 'grid-row--' + row }>
 			{children}
 		</div>
 	);
