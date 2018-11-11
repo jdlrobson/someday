2e03bbc97b2b63f3fd42984d0b7e3bbe763f9181
jdlrobson <jdlrobson@gmail.com>
2018-11-11 08:51:01 -0800

    Do not rely on mobile-sections api
    It's not updating and can be done easily using the parsoid html
    api. This removes some caching issues.
    
    Fixes: #6

---
+++ server/endpoints/htmlToJson.js
@@ -0,0 +1,73 @@
+import fetch from 'isomorphic-fetch';
+import domino from 'domino';
+function parsoidHTMLToJSON( html ) {
+	var doc = domino.createDocument( html );
+	const sections = Array.from( doc.querySelectorAll( 'section' ) ).map( ( sec ) => {
+		// Clone so you can make modifications
+		const secEl = sec.cloneNode( true );
+		let toclevel;
+		const h2 = secEl.querySelectorAll( 'h2,h3,h4,h5,h6' )[ 0 ];
+		const line = h2 ? h2.textContent : undefined;
+		const id = secEl.getAttribute( 'data-mw-section-id' );
+		const anchor = h2 ? h2.getAttribute( 'id' ) : 0;
+		// drop h2
+		if ( h2 ) {
+			h2.parentNode.removeChild( h2 );
+			// @todo 2 is not correct
+			switch ( h2.tagName ) {
+				case 'H6':
+					toclevel = 5;
+					break;
+				case 'H5':
+					toclevel = 4;
+					break;
+				case 'H4':
+					toclevel = 3;
+					break;
+				case 'H3':
+					toclevel = 2;
+					break;
+				default:
+					toclevel = 1;
+					break;
+			}
+		}
+		// Remove subsections
+		Array.from( secEl.querySelectorAll( 'section' ) ).forEach( ( node ) => node.parentNode.removeChild( node ) );
+		return {
+			toclevel,
+			anchor,
+			id,
+			line,
+			text: secEl.innerHTML
+		};
+	} );
+
+	const title = doc.querySelector( 'head title' ).textContent;
+	return {
+		lead: {
+			title,
+			displaytitle: title,
+			sections: sections.slice( 0, 1 )
+		},
+		remaining: {
+			sections: sections.slice( 1 )
+		}
+	};
+}
+
+export default function ( url ) {
+	return fetch( url, { redirect: 'manual' } )
+		.then( ( resp ) => {
+			const code = resp.status;
+			if ( [ 301, 302 ].indexOf( code ) > -1 ) {
+				return {
+					code,
+					title: resp.headers.get( 'Location' ).split( '/' ).slice( -1 )
+				};
+			} else {
+				return resp.text()
+					.then( ( html ) => parsoidHTMLToJSON( html ) );
+			}
+		} );
+}
--- server/endpoints/page.js
+++ server/endpoints/page.js
@@ -1,4 +1,3 @@
-import fetch from 'isomorphic-fetch';
 import domino from 'domino';
 
 import { SPECIAL_PROJECTS, HOST_SUFFIX, SITE_HOME } from './../config';
@@ -7,6 +6,7 @@ import undoLinkRewrite from './undoLinkRewrite';
 import extractHatnote from './extractHatnote';
 import extractInfobox from './extractInfobox';
 import getMedia from './extractMedia';
+import htmlToJson from './htmlToJson';
 
 function getBaseHost( lang, project ) {
 	if ( SPECIAL_PROJECTS.indexOf( project ) > -1 ) {
@@ -18,7 +18,7 @@ function getBaseHost( lang, project ) {
 
 export default function ( title, lang, project, revision ) {
 	const host = getBaseHost( lang, project ) + HOST_SUFFIX;
-	const path = '/api/rest_v1/page/mobile-sections/';
+	const path = '/api/rest_v1/page/html/';
 	const suffix = revision ? '/' + revision : '';
 	if ( title.substr( 0, 6 ) === 'Media:' ) {
 		title = title.replace( 'Media:', 'File:' );
@@ -27,63 +27,39 @@ export default function ( title, lang, project, revision ) {
 	var url = 'https://' + host + path +
     encodeURIComponent( title ) + suffix;
 
-	return fetch( url, { redirect: 'manual' } )
-		.then( function ( resp ) {
-			if ( [ 301, 302 ].indexOf( resp.status ) > -1 ) {
-				var redirectUrl = resp.headers.get( 'Location' );
-				if ( redirectUrl.indexOf( host ) > -1 ) {
-					return {
-						code: 301,
-						title: redirectUrl.replace( host, '' )
-							.replace( 'commons.wikimedia.org', '' )
-							.replace( path, '' ).replace( /https?\:\/\//, '' )
-					};
-				} else if ( redirectUrl.indexOf( 'commons.wikimedia.org' ) > -1 ) {
-					// Workaround for https://github.com/jdlrobson/weekipedia/issues/139
-					return {
-						code: 301,
-						project: 'en.commons',
-						title: redirectUrl
-							.replace( 'commons.wikimedia.org', '' )
-							.replace( path, '' ).replace( /https?\:\/\//, '' )
-					};
-				}
-			} else if ( resp.status === 200 ) {
-				return resp.json();
-			}
-		} ).then( function ( json ) {
-			if ( !json ) {
-				throw '404: Bad title given';
-			}
-			if ( json.code ) {
-				return json;
-			}
-
-			json.remaining.sections.forEach( function ( section ) {
-				if ( section.text ) {
-					var doc = domino.createDocument( section.text );
-					undoLinkRewrite( doc );
-					section.text = doc.body.innerHTML;
-				}
-			} );
-
-			// Workaround for https://phabricator.wikimedia.org/T145034
-			var doc = domino.createDocument( json.lead.sections.length && json.lead.sections[ 0 ] && json.lead.sections[ 0 ].text );
-			json.lead.media = getMedia( json.lead.sections.concat( json.remaining.sections ) );
-			if ( doc ) {
-				// See https://github.com/jdlrobson/weekipedia/issues/99 - preserve links in main page
-				if ( SITE_HOME.replace( /_/g, ' ' ) !== title.replace( /_/g, ' ' ) ) {
-					undoLinkRewrite( doc );
-				}
-				var infobox = extractInfobox( doc );
-				if ( !json.lead.mainpage ) {
-					var leadParagraph = extractLeadParagraph( doc );
-					json.lead.paragraph = leadParagraph;
-				}
-				json.lead.infobox = infobox;
-				json.lead.hatnote = extractHatnote( doc );
-				json.lead.sections[ 0 ].text = doc.body.innerHTML;
-			}
+	return htmlToJson( url ).then( function ( json ) {
+		if ( !json ) {
+			throw '404: Bad title given';
+		}
+		if ( json.code ) {
 			return json;
+		}
+
+		json.remaining.sections.forEach( function ( section ) {
+			if ( section.text ) {
+				var doc = domino.createDocument( section.text );
+				undoLinkRewrite( doc );
+				section.text = doc.body.innerHTML;
+			}
 		} );
+
+		// Workaround for https://phabricator.wikimedia.org/T145034
+		var doc = domino.createDocument( json.lead.sections.length && json.lead.sections[ 0 ] && json.lead.sections[ 0 ].text );
+		json.lead.media = getMedia( json.lead.sections.concat( json.remaining.sections ) );
+		if ( doc ) {
+			// See https://github.com/jdlrobson/weekipedia/issues/99 - preserve links in main page
+			if ( SITE_HOME.replace( /_/g, ' ' ) !== title.replace( /_/g, ' ' ) ) {
+				undoLinkRewrite( doc );
+			}
+			var infobox = extractInfobox( doc );
+			if ( !json.lead.mainpage ) {
+				var leadParagraph = extractLeadParagraph( doc );
+				json.lead.paragraph = leadParagraph;
+			}
+			json.lead.infobox = infobox;
+			json.lead.hatnote = extractHatnote( doc );
+			json.lead.sections[ 0 ].text = doc.body.innerHTML;
+		}
+		return json;
+	} );
 }
