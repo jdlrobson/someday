3ae9e1ca83769d14eeb73da90ea22572c4ea21d3
jdlrobson <jdlrobson@gmail.com>
2018-11-25 16:46:16 -0800

    babel-present-env (es5 transpiling)
    Fixes: #22

--- webpack.config.js
+++ webpack.config.js
@@ -52,23 +52,10 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
+				test: /\.jsx?$/,
+				exclude: /node_modules\/react-leaflet/,
 				use: [ {
-					loader: 'babel-loader',
-					query: {
-						presets: [ 'es2015', 'react' ]
-					}
-				} ]
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [ {
-					loader: 'babel-loader',
-					query: {
-						presets: [ 'es2015' ]
-					}
+					loader: 'babel-loader'
 				} ]
 			},
 			{
